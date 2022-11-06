8152bbf02431f14f614ba05b58c2d905802d8c02
Sharon <ss22361572ss@gmail.com>
2022-11-06 22:49:50 +0800

    More enemies & up-and-down spike

---
+++ Assets/Game_Demo/Scripts/Spike.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    //private PlayerMovement playerMovement;
+    
+    Vector2 start;
+    float i = 0;
+
+    void Start()
+    {
+        start = transform.position;
+        //playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        i += 0.01f;
+        transform.position = start + Vector2.up * Mathf.PingPong(i, (float) 1.5);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerMovement>().TakeDamage(3);
+        }
+    }
+
+}

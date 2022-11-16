665280379e194f4b5a5d6ae51988c368f6ebcf58
40947075s <40947075S@gapps.ntnu.edu.tw>
2022-11-16 17:53:48 +0800

    add boss health bar

---
+++ Assets/TestEnemy/Script/BossMeteor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMeteor : MonoBehaviour
+{
+    [Header ("Smoke status")]
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private int damage = 10;
+
+    private Vector3 player;
+
+    private void Awake() {
+        player = GameObject.FindGameObjectWithTag("Player").transform.position;
+    }
+
+    private void Update() {
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, player, speed * Time.deltaTime);
+        if(transform.localPosition == player){
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo) // when hit something
+    {
+        if (hitInfo.gameObject.tag == "Player") {
+            hitInfo.GetComponent<PlayerMovement>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}

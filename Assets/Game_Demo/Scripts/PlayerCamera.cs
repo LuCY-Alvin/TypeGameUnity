b059e56d90b86c772ee849bf0eb4b786b236efdd
Sharon <ss22361572ss@gmail.com>
2022-12-19 17:08:54 +0800

    guide camera to entrance/exit & add level2, 3 entrance/exit

--- Assets/Game_Demo/Scripts/PlayerCamera.cs
+++ Assets/Game_Demo/Scripts/PlayerCamera.cs
@@ -9,33 +9,40 @@ public class PlayerCamera : MonoBehaviour
     public GameObject player;
     public GameObject usualCamera;
     public GameObject castingCamera;
+    public GameObject exit;
     private static bool isBlurredBackground;
+    private bool isFollowPlayer;
 
     void Awake()
     {
         isBlurredBackground = false;
+        isFollowPlayer = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y+5, transform.position.z);
-        if (isBlurredBackground==false)
+        if (isFollowPlayer)
         {
-            if (GameObject.Find("CastPanel") != null)
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 5, transform.position.z);
+            if (isBlurredBackground == false)
             {
-                StartCoroutine(changeCamera(false, true, true));
+                if (GameObject.Find("CastPanel") != null)
+                {
+                    StartCoroutine(changeCamera(false, true, true));
+                }
+
             }
-            
-        }
-        else
-        {
-            if (GameObject.Find("CastPanel") == null)
+            else
             {
-                StartCoroutine(changeCamera(true, false, false));
+                if (GameObject.Find("CastPanel") == null)
+                {
+                    StartCoroutine(changeCamera(true, false, false));
+                }
             }
         }
         
+
     }
 
     IEnumerator changeCamera(bool isUsualCamera, bool isCastingCamera, bool isBlurred)
@@ -44,7 +51,29 @@ public class PlayerCamera : MonoBehaviour
         castingCamera.SetActive(isCastingCamera);
         isBlurredBackground = isBlurred;
         yield return null;
-        
+    }
+
+    IEnumerator LerpFromTo(Vector3 pos1, Vector3 pos2, float duration)
+    {
+        for (float t = 0f; t < duration; t += Time.deltaTime)
+        {
+            transform.position = Vector3.Lerp(pos1, pos2, t / duration);
+            yield return 0;
+        }
+    }
+
+    IEnumerator GoAndBack()
+    {
+        isFollowPlayer = false;
+        StartCoroutine(LerpFromTo(transform.position, new Vector3(exit.transform.position.x, exit.transform.position.y+5, transform.position.z), 2f));
+        yield return new WaitForSeconds(3f);
+        GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
+        isFollowPlayer = true;
+    }
+
+    public void smoothGuideCamera()
+    {
+        StartCoroutine(GoAndBack());
     }
 }
  
